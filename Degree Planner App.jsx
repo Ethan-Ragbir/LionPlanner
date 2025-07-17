a5533ce596f78fabcb0d34ba9a944114a00511e5
Ethan Ragbir <ragbire1@tcnj.edu>
2025-07-16 21:24:37 -0400

    Update Degree Planner App.jsx

--- Degree Planner App.jsx
+++ Degree Planner App.jsx
@@ -8,9 +8,9 @@ import SemesterPlanner from './components/SemesterPlanner';
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100">
+      <div className="min-h-screen bg-gray-50">
         <Navbar />
-        <div className="container mx-auto p-4">
+        <div className="container mx-auto px-6 py-8">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
